0020557e4662cb65c6b42ed826dc085dde24c656
yangit <2031974+yangit@users.noreply.github.com>
2023-02-18 00:43:10 +0800

    Add scripts

--- input/build.sh
+++ input/build.sh
@@ -10,4 +10,8 @@ node src/cli.js ./input/config-flactyl-thumb.yaml -o output/flactyl
 node ./input/copy_segments.js
 pkill -x pcbnew ||true
 open -n /Applications/KiCad/Pcbnew.app/ --args ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcbs/right.kicad_pcb
-# open -n /Applications/KiCad/Pcbnew.app/ --args ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcbs/thumb.kicad_pcb
+mkdir -p ~/Dropbox/github/keyboard/ergogen/output/flactyl/step
+# cd ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcb
+# /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcbs/left.kicad_pcb
+# /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcbs/right.kicad_pcb
+# /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step ~/Dropbox/github/keyboard/ergogen/output/flactyl/pcbs/thumb.kicad_pcb
