631564d1df02d1c2c87d765c6cebe098f9584e74
Yan <2031974+yangit@users.noreply.github.com>
2023-02-17 15:00:05 +0800

    Vias copied

--- input/copy_segments.js
+++ input/copy_segments.js
@@ -6,10 +6,13 @@ const segmentRegex = /\(segment .+\)\)\n/g
 fs.readdirSync(routed).filter((file) => file.endsWith(extension)).forEach(file => {
     console.log(file);
     const fileContents=fs.readFileSync(`${routed}/${file}`).toString('utf8').split('\n')
-    const segments=fileContents.filter(line => line.startsWith('  (segment (start'))
+    const segmentIndex = fileContents.findIndex(line => line.startsWith('  (segment (start'));
+    const segments = fileContents.slice(segmentIndex, fileContents.length)
+
+    
     const pcbFilePath = `${pcbs}/${file}`
     const pcbFile = fs.readFileSync(pcbFilePath).toString('utf8').split('\n')
-    pcbFile.splice(pcbFile.length - 3, 0, ...segments)    
+    pcbFile.splice(pcbFile.length - 3, 3, ...segments)    
 
     fs.writeFileSync(pcbFilePath,pcbFile.join('\n'));
 });
