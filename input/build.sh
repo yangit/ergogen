e0d18eb927fe5e1e2518bfe3f20541c6227f86e1
Yan <2031974+yangit@users.noreply.github.com>
2023-02-17 14:50:29 +0800

    Routing for left

--- input/build.sh
+++ input/build.sh
@@ -8,5 +8,5 @@ node src/cli.js ./input/config-flactyl-right.yaml -o output/flactyl
 node src/cli.js ./input/config-flactyl-thumb.yaml -o output/flactyl
 node ./input/copy_segments.js
 pkill -x pcbnew ||true
-# open -n /Applications/KiCad/Pcbnew.app/ --args ~/github/keyboard/ergogen/output/flactyl/pcbs/left.kicad_pcb
-open -n /Applications/KiCad/Pcbnew.app/ --args ~/github/keyboard/ergogen/output/flactyl/pcbs/thumb.kicad_pcb
+open -n /Applications/KiCad/Pcbnew.app/ --args ~/github/keyboard/ergogen/output/flactyl/pcbs/left.kicad_pcb
+# open -n /Applications/KiCad/Pcbnew.app/ --args ~/github/keyboard/ergogen/output/flactyl/pcbs/thumb.kicad_pcb
