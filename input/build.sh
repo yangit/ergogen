473cd747720eabc0c67a7017fa17ffa86dc891ac
Yan <2031974+yangit@users.noreply.github.com>
2023-05-30 17:49:15 +0800

    move files

--- input/build.sh
+++ input/build.sh
@@ -2,6 +2,7 @@ set -ex
 rm -rf ./output
 PCBS=("left" "right" "thumb" "test")
 LAYERS=F.Cu,F.Paste,F.SilkS,F.Mask,B.Cu,B.Paste,B.SilkS,B.Mask,Edge.Cuts
+OUTDIR=~/Dropbox/github/flactyl/pcb/ergogen/output
 
 mkdir -p ./output
 
@@ -17,21 +18,22 @@ node ./input/copy_tracks.js
 pkill -x pcbnew || true
 
 
-cd ~/Dropbox/github/keyboard/ergogen/output
+cd $OUTDIR
 for PCB in ${PCBS[@]}; do
-    mkdir -p ~/Dropbox/github/keyboard/ergogen/output/$PCB
-    cd ~/Dropbox/github/keyboard/ergogen/output/$PCB
-    /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step ~/Dropbox/github/keyboard/ergogen/output/pcbs/$PCB.kicad_pcb
+    mkdir -p $OUTDIR/$PCB
+    cd $OUTDIR/$PCB
+    /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step $OUTDIR/pcbs/$PCB.kicad_pcb
     mv ../outlines/${PCB}_* ./
     mv ../pcbs/${PCB}.kicad_pcb ./
     cp ../../input/bom/${PCB}.csv ./${PCB}_bom.csv
     mv ../../input/${PCB}.yaml ./
 done
-cd ~/Dropbox/github/keyboard/ergogen
+cd $OUTDIR
+cd ..
 node input/make_cpl.js
-open -n /Applications/KiCad/Pcbnew.app/ --args ~/Dropbox/github/keyboard/ergogen/output/left/left.kicad_pcb
-rm -rf ~/Dropbox/github/keyboard/ergogen/output/outlines
-rm -rf ~/Dropbox/github/keyboard/ergogen/output/pcbs
+open -n /Applications/KiCad/Pcbnew.app/ --args $OUTDIR/left/left.kicad_pcb
+rm -rf $OUTDIR/outlines
+rm -rf $OUTDIR/pcbs
 
 
 echo "\n\n\nAll done"
